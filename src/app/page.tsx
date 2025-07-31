bccc1d36e604f830664ebf7939c1972ee54f0ccc
AmodKumarJ <amodkumarjg@gmail.com>
2025-07-31 12:19:01 +0530

    update-v-0-0-2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const { userId } = auth();
+  const { userId } = await auth(); // <-- Added await
 
-  // If not logged in → go to sign-in
+ 
   if (!userId) {
-    redirect("/sign-in?redirect_url=/events");  // after login, go to /events
+    redirect("/sign-in?redirect_url=/events");
   }
 
-  // If logged in → go to events
+  
   redirect("/events");
 }
+
